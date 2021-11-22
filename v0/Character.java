f4b0b37653fc4a3d82291fa44fa56456f9dea9e3
Lauren Lee <llee30@stuy.edu>
2021-11-22 14:05:54 -0500

    update

---
+++ v0/Character.java
@@ -0,0 +1,18 @@
+public class Character{
+
+	public int HP;
+	public int strength;
+	public int defense;
+	public double atkRating;
+
+	public boolean isAlive(){
+	}
+
+	public int getDefense(){
+	}
+
+	public int lowerHP(int damage){
+	}
+
+	public int attack(char atkRating){
+	}
